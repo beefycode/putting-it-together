7162a634785fc21c1a381d438a9ba52bed754244
beefycode <koru.louis@gmail.com>
2023-01-18 14:08:51 +1300

    corrected name of useState method to setBirthday

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,14 +1,14 @@
 import React, {useState} from "react";
 
 const PersonCard = (props) => {
-    const [age,setPersonPokes] = useState(props.age);
+    const [age,setBirthday] = useState(props.age);
     const {firstName,lastName,hairColor} = props;
     return (
         <div className="Card">
             <h1>{firstName},{lastName}</h1>
             <p>Age: {age}</p>
             <p>hair Color: {hairColor}</p>
-            <button onClick={ (event) => setPersonPokes(age +1)}>Birthday Button for {firstName} {lastName}</button>
+            <button onClick={ (event) => setBirthday(age +1)}>Birthday Button for {firstName} {lastName}</button>
         </div>
     )
 }
